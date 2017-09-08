746ab81707bcc061af6e668d32772cc6e037ee17
Asher Shaheen <ashershaheen@gmail.com>
2017-09-07 22:47:38 -0400

    added form to add  URL

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,8 @@
 class UrlsController < ApplicationController
+
+    def create
+        url = params['url']
+        Url.create(url: url['url'], (description: url['description'])
+        redirect_to categories_path
+    end
 end

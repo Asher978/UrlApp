cd59b290d62d17f23cc9d82793382bcd689ac6a8
Asher Shaheen <ashershaheen@gmail.com>
2017-09-07 22:16:43 -0400

    added views, forms and methods for creating and showing categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,14 @@ class CategoriesController < ApplicationController
     def index 
         @category = Category.all
     end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def create
+        category = params['category']
+        Category.create(name: category['name'])
+        redirect_to categories_path
+    end
 end

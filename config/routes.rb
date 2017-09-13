a88f4df31e5aa83e8a6b8e8e6c47ca26059dfffc
Asher Shaheen <ashershaheen@gmail.com>
2017-09-12 21:36:52 -0400

    heroku deploy commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "categories#index"
+  # commentted out for heroku deployement
+  # root "categories#index" 
+  root 'welcome#index'
 
   resources :categories do
     resources :urls
